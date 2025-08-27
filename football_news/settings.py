37ebbd444322fd344f73092301c219c023252e95
naomi kyla <naomikyla@nomay-2.local>
2025-08-27 17:10:06 +0700

    Tutorial 0

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,8 +31,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ...
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naomi-kyla-footballnews.pbp.cs.ui.ac.id"]
 ...
+
 
 
 # Application definition
